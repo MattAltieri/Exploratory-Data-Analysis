b79d0def92699fc8f95d5fc331a5e275893636f0
MattAltieri <matt.onlystory@gmail.com>
2015-05-02 15:41:10 -0400

    Finished EDA Air Pollution Case Study.

--- Case Studies/Air Pollution Case Study.R
+++ Case Studies/Air Pollution Case Study.R
@@ -50,3 +50,76 @@ str(dates)
 
 hist(dates, "month")
 hist(dates[negative], "month")
+
+# Find a monitor in both datasets
+site0 <- unique(subset(pm0, State.Code == 36, c(County.Code, Site.ID)))
+site1 <- unique(subset(pm1, State.Code == 36, c(County.Code, Site.ID)))
+
+head(site0)
+
+site0 <- paste(site0[,1], site0[,2], sep=".")
+site1 <- paste(site1[,1], site1[,2], sep=".")
+
+str(site0)
+str(site1)
+
+both <- intersect(site0, site1)
+both
+
+pm0$county.site <- with(pm0, paste(County.Code, Site.ID, sep="."))
+pm1$county.site <- with(pm1, paste(County.Code, Site.ID, sep="."))
+cnt0 <- subset(pm0, State.Code == 36 & county.site %in% both)
+cnt1 <- subset(pm1, State.Code == 36 & county.site %in% both)
+head(cnt0)
+head(cnt1)
+
+sapply(split(cnt0, cnt0$county.site), nrow)
+
+
+pm1sub <- subset(pm1, State.Code == 36 & County.Code == 63 & Site.ID == 2008)
+pm0sub <- subset(pm0, State.Code == 36 & County.Code == 63 & Site.ID == 2008)
+dim(pm1sub)
+dim(pm0sub)
+
+dates1 <- ymd(pm1sub$Date)
+x1sub <- pm1sub$Sample.Value
+plot(dates1, x1sub)
+
+dates0 <- ymd(pm0sub$Date)
+x0sub <- pm0sub$Sample.Value
+plot(dates0, x0sub)
+
+par(mfrow=c(1, 2), mar=c(4, 4, 2, 1))
+plot(dates0, x0sub, pch=20)
+abline(h=median(x0sub, na.rm=T))
+plot(dates1, x1sub, pch=20)
+abline(h=median(x1sub, na.rm=T))
+
+rng <- range(x0sub, x1sub, na.rm=T)
+
+par(mfrow=c(1, 2), mar=c(4, 4, 2, 1))
+plot(dates0, x0sub, pch=20, ylim=rng)
+abline(h=median(x0sub, na.rm=T))
+plot(dates1, x1sub, pch=20, ylim=rng)
+abline(h=median(x1sub, na.rm=T))
+
+head(pm0)
+
+mn0 <- with(pm0, tapply(Sample.Value, State.Code, mean, na.rm=T))
+str(mn0)
+summary(mn0)
+
+mn1 <- with(pm1, tapply(Sample.Value, State.Code, mean, na.rm=T))
+summary(mn1)
+
+d0 <- data.frame(state=names(mn0), mean=mn0)
+d1 <- data.frame(state=names(mn1), mean=mn1)
+head(d0)
+head(d1)
+mrg <- merge(d0, d1, by="state")
+dim(mrg)
+head(mrg)
+par(mfrow=c(1, 1))
+with(mrg, plot(rep(1999, 53), mrg[,2], xlim=c(1998, 2013)))
+with(mrg, points(rep(2012, 53), mrg[,3], xlim=c(1998, 2013)))
+segments(rep(1999, 53), mrg[,2], rep(2012, 53), mrg[,3])
